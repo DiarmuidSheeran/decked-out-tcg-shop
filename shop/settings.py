8a0f1e192caf871376eff9ef4646113a66193942
Diarmuid Sheeran <dssheeran@outlook.com>
2023-12-02 16:16:33 +0000

    Create env file and create new secret key, add new secret key to env file and call the key from settings file

--- shop/settings.py
+++ shop/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+vn8*f)kws5$54@wtv1=h)7yl$6#1#z*l47lq42rx63&i+6sf='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
