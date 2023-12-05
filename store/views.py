efec706386c89aca4dbe4e0ce7abd3d310fa1b44
Diarmuid Sheeran <dssheeran@outlook.com>
2023-12-05 21:11:13 +0000

    Add property decorator to toals methods in model and update cart template to show cart items pricing

--- store/views.py
+++ store/views.py
@@ -17,9 +17,13 @@ def cart(request):
 	    items = order.orderitem_set.all()
     else:
 		#Create empty cart for now for non-logged in user
-	    items = []
+    	items = []
+    	order = {'get_cart_total':0, 'get_cart_items':0}
+        
 
-    context = {'items':items}
+    context = {
+        'items':items,
+        'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
