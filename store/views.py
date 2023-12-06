9e346f4a43a839edecccd42f0aac3876706ddd09
Diarmuid Sheeran <dssheeran@outlook.com>
2023-12-06 23:02:53 +0000

    update store view to take in the cart quantities

--- store/views.py
+++ store/views.py
@@ -6,9 +6,22 @@ import json
 
 def store(request):
 
+    if request.user.is_authenticated:
+	    customer = request.user.customer
+	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	    items = order.orderitem_set.all()
+	    cartItems = order.get_cart_items
+    else:
+	    items = []
+	    order = {'get_cart_total':0, 'get_cart_items':0}
+	    cartItems = order['get_cart_items']
+
     products = Product.objects.all()
 
-    context = {'products':products}
+    context = {
+        'products':products,
+        'cartItems':cartItems,
+        }
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -66,4 +79,4 @@ def updateItem(request):
 
     if orderItem.quantity <= 0:
 	    orderItem.delete()
-return JsonResponse('Item was added', safe=False)
+    return JsonResponse('Item was added', safe=False)
