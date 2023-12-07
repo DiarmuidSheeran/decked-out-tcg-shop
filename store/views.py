20f74224dadaef0f57d85939bc7c1364d52c386b
Diarmuid Sheeran <dssheeran@outlook.com>
2023-12-07 20:45:53 +0000

    Add script to checkout to remove adress field if shipping is false

--- store/views.py
+++ store/views.py
@@ -27,34 +27,42 @@ def store(request):
 def cart(request):
 
     if request.user.is_authenticated:
-	    customer = request.user.customer
-	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
-	    items = order.orderitem_set.all()
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
 		#Create empty cart for now for non-logged in user
-    	items = []
-    	order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
         
 
     context = {
         'items':items,
-        'order':order}
+        'order':order,
+        'cartItems':cartItems
+        }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
     if request.user.is_authenticated:
-	    customer = request.user.customer
-	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
-	    items = order.orderitem_set.all()
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
 		#Create empty cart for now for non-logged in user
-    	items = []
-    	order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order = {'get_cart_total'}
         
 
     context = {
         'items':items,
-        'order':order}
+        'order':order,
+        'cartItems':cartItems
+        }
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
