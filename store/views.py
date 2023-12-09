a4466fd743da9ffa239b4194deed51ec4d6bd0f8
Diarmuid Sheeran <dssheeran@outlook.com>
2023-12-09 10:44:27 +0000

    Update store view to to use cookieCart function

--- store/views.py
+++ store/views.py
@@ -9,14 +9,16 @@ from .utils import cookieCart
 def store(request):
 
     if request.user.is_authenticated:
-	    customer = request.user.customer
-	    order, created = Order.objects.get_or_create(customer=customer, complete=False)
-	    items = order.orderitem_set.all()
-	    cartItems = order.get_cart_items
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
-	    items = []
-	    order = {'get_cart_total':0, 'get_cart_items':0}
-	    cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+
 
     products = Product.objects.all()
 
